0b039b3d315725ea89e4195137fb45a2cbf07702
Giacomo Feltrin <feltrin.gi@gmail.com>
2023-11-01 22:50:35 +0100

    fix: return json from scraping

--- animeita.py
+++ animeita.py
@@ -8,6 +8,7 @@ def get_animesaturn():
 
     # Make an HTTP GET request to fetch the HTML content
     response = requests.get(url)
+    anime_data = []
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
@@ -17,49 +18,27 @@ def get_animesaturn():
         soup = BeautifulSoup(html_content, 'html.parser')
 
         # Find and extract the desired information (as shown in the previous code)
+        anime_cards = soup.find_all(class_='anime-card-newanime')
 
-        anime_data = []
+        for card in anime_cards:
+            # Extract anime title and URL
+            title = card.find('a', title=True).get('title')
+            url = card.find('a', href=True).get('href')
+            image_url = card.find('img', class_='new-anime').get('src')
 
-        anime_genres = soup.find_all('div', class_='genres-rows')
-
-        for genre_row in anime_genres:
-            genre_name = genre_row.find('h2').text.strip()
-
-            movies = []
-            movie_cards = genre_row.find_all('div', class_='anime-card-newanime main-anime-card')
-
-            for card in movie_cards:
-                # Extract anime title and URL
-                title = card.find('a', title=True).get('title')
-                url = card.find('a', href=True).get('href')
-                image_url = card.find('img', class_='new-anime').get('src')
-
-                # Example data structure for a single movie
-                movie_data = {
-                    "title": title,
-                    "url": url,
-                    "poster": image_url,
-                    "plot": '',  # You can add plot information if available
-                    "year": None  # You can add the year if available
-                }
-
-                movies.append(movie_data)
-
-            genre_data = {
-                'genre': genre_name,
-                'icon': '',  # You can set the icon path if needed
-                'fanart': '',  # You can set the fanart path if needed
-                'movies': movies
+            # Example data structure for a single movie
+            single_data = {
+                "title": title,
+                "url": url,
+                "poster": image_url,
+                "plot": '',
+                "year": None
             }
 
-            anime_data.append(genre_data)
+            anime_data.append(single_data)
 
+        print(anime_data)
         return anime_data
     else:
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
         return None
-
-anime_data = get_animesaturn()
-VIDEOS = anime_data
-
-print(anime_data)
