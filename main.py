41f7b6e2c41ebdf4658133510a1683aec9d603b3
Giacomo Feltrin <feltrin.gi@gmail.com>
2023-11-02 23:20:02 +0100

    fix: correctly pass user input

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ def get_avideos(abutton):
         query = get_user_input() # User input via onscreen keyboard
         if not query:
             return [] # Return empty list if query is blank
-        subpath = "animelist?search=".format(quote(query))
+        subpath = "animelist?search={}".format(quote(query))
         videos = get_animesaturn_search(subpath)
         return videos
 
